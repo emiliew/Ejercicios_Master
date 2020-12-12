71397ea5915e53d3cd0a01b5ff19c7f27136044e
emiliew <emiliomolinaharo@outlook.com>
2020-12-12 18:57:30 +0100

    Prueba JSON

---
+++ prueba_JSON.py
@@ -0,0 +1,24 @@
+import requests
+
+# función que imprime por pantalla cada uno de los comentarios
+def printComment(comment):
+    print('\tcomment[id] =', comment['id'])
+    print('\tcomment[body] =', comment['body'])
+    print('\tcomment[name] =', comment['name'])
+    print('\tcomment[email] =', comment['email'])
+    print('\tcomment[postId] =', comment['postId'])
+
+# función que imprime por pantalla todos los comentarios
+def printComments(comments):
+    for i in range(len(comments)):
+        print('i =', i)
+        printComment(comments[i])
+
+url='https://jsonplaceholder.typicode.com/posts/1/comments'
+
+# hacemos un petición GET a la URL
+response = requests.get(url)
+
+# convierte la respuesta a un diccionario Python
+jsonList = response.json()
+printComments(jsonList)
